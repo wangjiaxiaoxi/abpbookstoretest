d59cf265c7152d263532c26b65532ffd025023cd
xiaoxi <xiaoxiw@ciandt.com>
2020-11-04 15:48:04 +0800

    init project

---
+++ wxxabp/aspnet-core/src/Acme.BookStore.Domain/BookStoreConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace Acme.BookStore
+{
+    public static class BookStoreConsts
+    {
+        public const string DbTablePrefix = "Bs";
+
+        public const string DbSchema = null;
+    }
+}
---
+++ wxxabp/aspnet-core/src/Acme.BookStore.EntityFrameworkCore.DbMigrations/Migrations/20201104072624_update-book.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class updatebook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubName",
+                table: "BsBooks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubName",
+                table: "BsBooks");
+        }
+    }
+}
