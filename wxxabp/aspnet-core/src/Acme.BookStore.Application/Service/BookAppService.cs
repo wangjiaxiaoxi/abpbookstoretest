ef417b496779b33be67678f0b6f0ccea6882e526
xiaoxi <xiaoxiw@ciandt.com>
2020-11-04 16:21:01 +0800

    AutoMapper and dto

---
+++ wxxabp/aspnet-core/src/Acme.BookStore.Application/Service/BookAppService.cs
@@ -0,0 +1,28 @@
+﻿using Acme.BookStore.Books;
+using Acme.BookStore.Dtos;
+using Acme.BookStore.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Service
+{
+    public class BookAppService :
+        CrudAppService<
+            Book, //The Book entity
+            BookDto, //Used to show books
+            Guid, //Primary key of the book entity
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateBookDto>, //Used to create/update a book
+        IBookAppService //implement the IBookAppService
+    {
+        public BookAppService(IRepository<Book, Guid> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
